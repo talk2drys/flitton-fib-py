52a455cb3c59227619f5b1849dbb1ac3b9edd789
Daniel Temiagin <daniel.temiagin@gmail.com>
2022-04-20 16:06:32 -0600

    added simple functon

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import find_packages, setup
 
+
+# Read the readme file into a string variable inject
+# into the long_description paramter of our setup function
+# call
+with open("README.md", "r") as file_handler:
+    long_description: str = file_handler.read()
+
 setup(
         name="flitton_fib_py",
         version="0.0.1",
         author=" Daniel Temiagin",
         author_email="danile.temiagin@gmail.com",
         description="Calculate a Fibonacci number",
-        long_description="A basic library that calculates Fibonacci numbers",
+        long_description=long_description,
         long_description_content_type="text/markdown",
         url="https://github.com/talk2drys/flitton-fib-py",
         install_requires=[],
@@ -16,6 +23,6 @@ setup(
             "Programjing Language :: Python 3",
             "Operating System :: OS Independent",
             ],
-        python_requires='>=-3'
+        python_requires='>=-3',
         test_requires=["pytest"],
         )
